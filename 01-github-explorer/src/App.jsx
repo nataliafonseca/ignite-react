7bbb77f89f9c1108400762bd4549860e49d85486
Natália Fonseca <nataliabf@outlook.com>
2021-03-09 12:53:15 -0300

    Fundamentos - Chamadas HTTP

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,11 +1,5 @@
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './components/Counter';
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
