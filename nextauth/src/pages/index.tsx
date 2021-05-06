bb92bb2dbb3b6436169a481056e72ab1f4f70fcb
Natália B. Fonseca <nataliabf@outlook.com>
2021-05-06 10:37:58 -0300

    Autenticação e autorização: NextAuth - Autenticação no server-side

--- nextauth/src/pages/index.tsx
+++ nextauth/src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -29,3 +32,9 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
