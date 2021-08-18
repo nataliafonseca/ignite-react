10fd714c21d015e7a53ea1d0abfac1c530c56678
Natália B. Fonseca <nataliabf@outlook.com>
2021-08-18 20:52:53 -0300

    Projeto Chakra: DashGO - Finalizando a aplicação: Mutations

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -1,11 +1,15 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 import * as yup from 'yup';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
 
 type CreateUserFormData = {
   name: string;
@@ -20,15 +24,32 @@ const createUserFormSchema = yup.object().shape({
   password: yup.string().required('O campo senha é obrigatório.').min(6, 'A senha deve conter ao menos 6 caracteres.'),
   password_confirmation: yup.string().oneOf([null, yup.ref('password')], 'As senhas não conferem.'),
 });
+
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('/users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      },
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({ resolver: yupResolver(createUserFormSchema) });
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values, event) => {
-    event.preventDefault();
+    await createUser.mutateAsync(values);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(values);
+    router.push('/users')
   };
 
   return (
