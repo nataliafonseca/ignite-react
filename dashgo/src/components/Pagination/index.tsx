e227169d533aed1bfbbb657ef10a0d300b01acba
Natália B. Fonseca <nataliabf@outlook.com>
2021-08-18 01:13:39 -0300

    Projeto Chakra: DashGO - Responsividade: Listagem de usuários

--- dashgo/src/components/Pagination/index.tsx
+++ dashgo/src/components/Pagination/index.tsx
@@ -1,20 +1,20 @@
-import { Box, HStack } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import { PaginationItem } from './PaginationItem';
 
 export function Pagination() {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack mt="8" justify="space-between" align="center" spacing="6" direction={["column", "row"]}>
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
-      <HStack spacing="2">
+      <Stack spacing="2" direction="row">
         <PaginationItem number={1} isCurrent />
         <PaginationItem number={2} />
         <PaginationItem number={3} />
         <PaginationItem number={4} />
         <PaginationItem number={5} />
         <PaginationItem number={6} />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   );
 }
--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,17 +1,37 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
       <Flex width="100%" my="6" maxWidth="1480px" mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius="8px" bg="gray.800" p="8">
+        <Box flex="1" borderRadius="8px" bg="gray.800" p={['6', '8']}>
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
@@ -25,17 +45,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
-                <Th width="8"></Th>
+                {isWideVersion && <Th>Data de Cadastro</Th>}
+                {isWideVersion && <Th width="8"></Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -46,12 +66,56 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} />}>
-                    Editar
-                  </Button>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} />}>
+                      Editar
+                    </Button>
+                  </Td>
+                )}
+              </Tr>
+              <Tr>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
                 </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Natália Fonseca</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      nataliabf@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} />}>
+                      Editar
+                    </Button>
+                  </Td>
+                )}
+              </Tr>
+              <Tr>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Natália Fonseca</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      nataliabf@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} />}>
+                      Editar
+                    </Button>
+                  </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
