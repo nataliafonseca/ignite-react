e3d5588e508ed8856eccf397de6a8a26737cb3c4
Natália Fonseca <nataliabf@outlook.com>
2021-10-28 10:32:16 -0300

    Performance: PerfReact - Utilizando o useMemo

--- perfreact/components/SearchResults.tsx
+++ perfreact/components/SearchResults.tsx
@@ -1,21 +1,28 @@
-import { ProductItem } from './ProductItem';
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
   results: Array<{
     id: number;
     price: number;
     title: string;
-  }>
+  }>;
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
-  <div>
-    {results.map(product => {
-      return (
-        <ProductItem key={product.id} product={product} />
-      );
-    })}
-  </div>
-  )
+    <div>
+      <h2>{totalPrice}</h2>
+
+      {results.map((product) => {
+        return <ProductItem key={product.id} product={product} />;
+      })}
+    </div>
+  );
 }
