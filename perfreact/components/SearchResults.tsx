c1fa8ab82464dd36b59319ca2d68c36652aafcb5
Natália Fonseca <nataliabf@outlook.com>
2021-10-28 12:40:37 -0300

    Performance: PerfReact - Virtualização

--- perfreact/components/SearchResults.tsx
+++ perfreact/components/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { ProductItem } from "./ProductItem";
+import { List, ListRowRenderer } from "react-virtualized";
 
 interface SearchResultsProps {
   results: Array<{
@@ -16,19 +17,26 @@ export function SearchResults({
   onAddToWishlist,
   totalPrice,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishlist={onAddToWishlist}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
